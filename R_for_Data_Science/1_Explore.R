2f6e2ed992ff47297fff60af623dae52dae279b5
chinhungtseng <bluesshown@gmail.com>
2019-02-26 22:20:55 +0800

    update

--- R_for_Data_Science/1_Explore.R
+++ R_for_Data_Science/1_Explore.R
@@ -1,5 +1,3 @@
-setwd('/Users/peter/Documents/Project/Notebook_with_R/R_for_Data_Science/')
-getwd()
 # This is the note with book: R for Data Science
 # https://r4ds.had.co.nz/explore-intro.html
 
@@ -2170,11 +2168,33 @@ diamonds %>%
 ## Where does your analysis “live”?
 
 # 8.1 What is real?
+## As a beginning R user, it’s OK to consider your environment 
+## (i.e. the objects listed in the environment pane) “real”. 
+## However, in the long run, you’ll be much better off if you consider your R scripts as “real”.
 
+## There is a great pair of keyboard shortcuts that will work together to 
+## make sure you’ve captured the important parts of your code in the editor:
 
+# Press Cmd/Ctrl + Shift + F10 to restart RStudio.
+# Press Cmd/Ctrl + Shift + S to rerun the current script.
 
+# 8.2 Where does your analysis live?
+getwd()
 
+# 8.3 Paths and directories
 
+# 8.4 RStudio projects
+
+# 8.5 Summary
+# In summary, RStudio projects give you a solid workflow that will serve you well in the future:
+  
+# 1. Create an RStudio project for each data analysis project.
+# 2. Keep data files there; we’ll talk about loading them into R in data import.
+# 3. Keep scripts there; edit them, run them in bits or as a whole.
+# 4. Save your outputs (plots and cleaned data) there.
+# 5. Only ever use relative paths, not absolute paths.
+
+# Everything you need is in one place, and cleanly separated from all the other projects that you are working on.
 
 
 
---
+++ R_for_Data_Science/2_Wrangle.R
@@ -0,0 +1,16 @@
+## Wrangle: https://r4ds.had.co.nz/wrangle-intro.html
+
+# 9 Introduction
+# This part of the book proceeds as follows:
+
+## 1. In tibbles, you’ll learn about the variant of the data frame that we use in this book: the tibble. 
+##    You’ll learn what makes them different from regular data frames, and how you can construct them “by hand”.
+
+## 2. In data import, you’ll learn how to get your data from disk and into R. We’ll focus on plain-text rectangular formats, 
+##    but will give you pointers to packages that help with other types of data.
+
+## 3. In tidy data, you’ll learn about tidy data, a consistent way of storing your data that makes transformation, 
+##    visualisation, and modelling easier. You’ll learn the underlying principles, and how to get your data into a tidy form.
+
+
+
