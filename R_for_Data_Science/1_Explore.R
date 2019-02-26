73ac13205e99902673b9ce596593352a3e3e462e
chinhungtseng <bluesshown@gmail.com>
2019-02-26 12:41:44 +0800

    update

--- R_for_Data_Science/1_Explore.R
+++ R_for_Data_Science/1_Explore.R
@@ -1394,13 +1394,10 @@ flights_sml %>%
 
 ## 2. Which plane (tailnum) has the worst on-time record?
 flights %>% 
+  filter(dep_delay > 0) %>% 
   group_by(tailnum) %>% 
-  summarise(
-    prop_on_time = sum(arr_delay <= 60, na.rm = TRUE) / n(),
-    mean_arr_delay = mean(arr_delay, na.rm = TRUE),
-    flights = n()
-  ) %>% 
-  arrange(prop_on_time, desc(mean_arr_delay))
+  summarise(avg_delay = mean(dep_delay)) %>% 
+  arrange(desc(avg_delay))
 
 ## 3. What time of day should you fly if you want to avoid delays as much as possible?
 flights %>% 
@@ -1787,6 +1784,7 @@ ggplot(data = mpg) +
 # 7.5.1.1 Exercises
 # 1. Use what you’ve learned to improve the visualisation of 
 #    the departure times of cancelled vs. non-cancelled flights.
+## original
 nycflights13::flights %>% 
   mutate(
     cancelled = is.na(dep_time),
@@ -1795,7 +1793,17 @@ nycflights13::flights %>%
     sched_dep_time = sched_hour + sched_min / 60
   ) %>% 
   ggplot(mapping = aes(x = sched_dep_time)) +
-    geom_freqpoly(mapping = aes(color = cancelled))
+    geom_freqpoly(mapping = aes(color = cancelled), binwidth = .25)
+
+nycflights13::flights %>% 
+  mutate(
+    cancelled = is.na(dep_time),
+    sched_hour = sched_dep_time %/% 100,
+    sched_min = sched_dep_time %% 100,
+    sched_dep_time = sched_hour + sched_min / 60
+  ) %>% 
+  ggplot(mapping = aes(x = sched_dep_time, y = ..density..)) +
+    geom_freqpoly(mapping = aes(color = cancelled), binwidth = .25)
 
 nycflights13::flights %>% 
   mutate(
@@ -1876,30 +1884,297 @@ nycflights13::flights %>%
 #    display a prohibitively large number of “outlying values”. One approach to remedy this problem is the letter value plot. 
 #    Install the lvplot package, and try using geom_lv() to display the distribution of price vs cut. What do you learn? 
 #    How do you interpret the plots?
+ggplot(data = diamonds) + 
+  geom_boxplot(mapping = aes(x = cut, y = price))
 
+if(!require(lvplot)) install.packages('lvplot')
+library(lvplot)
 
-
-
-
-
+ggplot(data = diamonds, mapping = aes(x = cut, y = price)) + 
+  geom_lv()
+## Reference: letter value boxplot
+## https://www.r-project.org/conferences/useR-2006/Slides/HofmannEtAl.pdf
 
 # 5. Compare and contrast geom_violin() with a facetted geom_histogram(), or a coloured geom_freqpoly(). 
 #    What are the pros and cons of each method?
+## geom_violin()
+ggplot(data = diamonds) + 
+  geom_violin(mapping = aes(x = cut, y = price)) + 
+  coord_flip()
 
+## geom_histogram() with faceted
+ggplot(data = diamonds) + 
+  geom_histogram(mapping = aes(x = price, fill = cut), show.legend = FALSE, binwidth = 20) + 
+  facet_grid(cut ~ .)
 
+## coloured geom_freqpoly()
+ggplot(data = diamonds) + 
+  geom_freqpoly(mapping = aes(x = price, color = cut))
 
-
-
-
+## referenct: https://lokhc.wordpress.com/r-for-data-science-solutions/chapter-7-exploratory-data-analysis/
 
 # 6. If you have a small dataset, it’s sometimes useful to use geom_jitter() to see the relationship between a continuous 
 #    and categorical variable. The ggbeeswarm package provides a number of methods similar to geom_jitter().
 #    List them and briefly describe what each one does.
+if(!require(ggbeeswarm)) install.packages('ggbeeswarm')
+library(ggbeeswarm)
+## original
+ggplot(data = mpg, mapping = aes(x = cyl, y = hwy)) +
+  geom_point()
+
+## same as original
+ggplot(data = mpg, mapping = aes(x = cyl, y = hwy)) + 
+  geom_jitter()
+
+## (1) geom_quasirandom
+## (2) geom_beeswarm
+ggplot(data = mpg, mapping = aes(x = cyl, y = hwy)) + 
+  geom_beeswarm()
+
+ggplot(data = mpg, mapping = aes(x = cyl, y = hwy)) + 
+  geom_quasirandom()
 
 
+# 7.5.2 Two categorical variables
+## To visualise the covariation between categorical variables, 
+## you’ll need to count the number of observations for each combination. 
+
+## geom_count()
+ggplot(data = diamonds) + 
+  geom_count(mapping = aes(x = cut, y = color))
+
+## compute the count with dplyr
+diamonds %>% 
+  count(color, cut)
+
+## visualise with geom_tile() and the fill aesthetic
+diamonds %>% 
+  count(color, cut) %>% 
+  ggplot(mapping = aes(x = color, y = cut)) + 
+  geom_tile(mapping = aes(fill = n))
+
+## For larger plots, you might want to try the d3heatmap or heatmaply packages, which create interactive plots.
+## https://github.com/rstudio/d3heatmap
+if (!require("devtools")) install.packages("devtools")
+devtools::install_github("rstudio/d3heatmap")
+library(d3heatmap)
+d3heatmap(mtcars, scale = "column", colors = "Spectral")
+
+## https://github.com/talgalili/heatmaply
+if(!require('heatmaply')) install.packages('heatmaply')
+library(heatmaply)
+heatmaply(mtcars, k_row = 3, k_col = 2)
+
+# 7.5.2.1 Exercises
+# 1. How could you rescale the count dataset above to more clearly 
+#    show the distribution of cut within colour, or colour within cut?
+## original
+ggplot(data = diamonds) + 
+  geom_count(mapping = aes(x = color, y = cut))
+## change the scale
+diamonds %>% 
+  count(color, cut) %>% 
+  group_by(color) %>% 
+  mutate(prop = n / sum(n)) %>% 
+  ggplot(mapping = aes(x = color, y = cut, fill = prop)) + 
+  geom_tile()
+
+# 2. Use geom_tile() together with dplyr to explore how average flight 
+#    delays vary by destination and month of year. 
+#    What makes the plot difficult to read? How could you improve it?
+
+## (1) how average flight delays vary by destination and month of year. 
+nycflights13::flights %>% 
+  group_by(dest, month) %>% 
+  summarise(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) %>% 
+  ggplot(mapping = aes(x = factor(month), y = dest, fill = avg_dep_delay)) + 
+  geom_tile()
+
+## (2) How could you improve it?
+nycflights13::flights %>% 
+  group_by(dest, month) %>% 
+  summarise(avg_dep_delay = mean(dep_delay, na.rm = TRUE)) %>% 
+  ungroup() %>% 
+  group_by(dest) %>% 
+  mutate(n_month = n()) %>% 
+  ggplot(mapping = aes(x = factor(month), 
+                       y = reorder(dest, n_month), 
+                       fill = avg_dep_delay)) + 
+  geom_tile() + 
+  scale_fill_gradient2(low = 'yellow', mid = 'orange', high = 'red', midpoint = 35)
+  
+# 3. Why is it slightly better to use aes(x = color, y = cut) rather than 
+#    aes(x = cut, y = color) in the example above?
+diamonds %>% 
+  count(color, cut) %>% 
+  ggplot() + 
+  geom_tile(mapping = aes(x = color, y = cut, fill = n))
+
+# 7.5.3 Two continuous variables
+# geom_point(): relationship between the carat size and price of a diamond.
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price))
+
+# Scatterplots become less useful as the size of your dataset grows, because points begin to overplot, 
+# and pile up into areas of uniform black (as above). You’ve already seen one way to fix the problem:
+# using the alpha aesthetic to add transparency.
+ggplot(data = diamonds) + 
+  geom_point(mapping = aes(x = carat, y = price), alpha = .01)
+
+# But using transparency can be challenging for very large datasets. Another solution is to use bin.
+# Previously you used geom_histogram() and geom_freqpoly() to bin in one dimension. 
+# Now you’ll learn how to use geom_bin2d() and geom_hex() to bin in two dimensions.
+ggplot(data = smaller) + 
+  geom_bin2d(mapping = aes(x = carat, y = price))
+
+ggplot(data = smaller) + 
+  geom_hex(mapping = aes(x = carat, y = price))
+
+# Another option is to bin one continuous variable so it acts like a categorical variable.
+ggplot(data = smaller, mapping = aes(x = carat, y = price)) + 
+  geom_boxplot(mapping = aes(group = cut_width(carat, 0.1)))
+
+## cut_width(x, width), as used above, divides x into bins of width width.
+## it’s difficult to tell that each boxplot summarises a different number of points. 
+## One way to show that is to make the width of the boxplot proportional to the number of points with varwidth = TRUE.
+
+ggplot(data = smaller, mapping = aes(x = carat, y = price)) + 
+  geom_boxplot(mapping = aes(group = cut_width(carat, 0.1)), varwidth = TRUE)
+
+# Another approach is to display approximately the same number of points in each bin. That’s the job of cut_number():
+ggplot(data = smaller) + 
+  geom_boxplot(mapping = aes(x = carat, y = price, group = cut_number(carat, 20)))
+
+# 7.5.3.1 Exercises
+# 1. Instead of summarising the conditional distribution with a boxplot,
+#    you could use a frequency polygon. What do you need to consider when using cut_width() vs cut_number()? 
+#    How does that impact a visualisation of the 2d distribution of carat and price?
+ggplot(data = smaller, mapping = aes(x = price)) + 
+  geom_freqpoly(mapping = aes(color = cut_width(carat, .2)), bins = 20)
+
+ggplot(data = smaller) + 
+  geom_freqpoly(mapping = aes(x = price, color = cut_number(carat, 10)), bins = 20)
+
+?cut_number
+## cut_number makes n groups with (approximately) equal numbers of observations; 
+## cut_width makes groups of width width.
+
+ggplot(data = diamonds) + 
+  geom_bin2d(mapping = aes(x = price, y = carat, fill = cut_width(carat, .4)))
+
+ggplot(data = diamonds) + 
+  geom_bin2d(mapping = aes(x = price, y = carat, fill = cut_number(carat, 12)))
+
+# 2. Visualise the distribution of carat, partitioned by price.
+ggplot(data = diamonds, y = ..density..) + 
+  geom_freqpoly(mapping = aes(x = carat, color = cut_number(price, 5)), bins = 30)
+
+# 3. How does the price distribution of very large diamonds compare to small diamonds? 
+#    Is it as you expect, or does it surprise you?
+ggplot(data = diamonds) + 
+  geom_boxplot(mapping = aes(x = price, y = carat, color = cut_number(price, 10)))
+## The price distribytion of large diamonds are much more variable the small diamonds.
+
+# 4. Combine two of the techniques you’ve learned to visualise the combined distribution of cut, carat, and price.
+ggplot(data = diamonds) + 
+  geom_point(mapping = aes(x = carat, y = price, color = cut), alpha = .25)
+
+ggplot(data = diamonds) + 
+  geom_boxplot(mapping = aes(x = cut, y = price, color = cut_number(carat, 5)))
+
+ggplot(data = diamonds) + 
+  geom_bin2d(mapping = aes(x = price, y = cut, fill = cut_number(carat, 5)))
 
 
+# 5. Two dimensional plots reveal outliers that are not visible in one dimensional plots. 
+#    For example, some points in the plot below have an unusual combination of x and y values, 
+#    which makes the points outliers even though their x and y values appear normal when examined separately.
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = x, y = y)) +
+  coord_cartesian(xlim = c(4, 11), ylim = c(4, 11))
+
+# 7.6 Patterns and models
+## Patterns in your data provide clues about relationships. If a systematic relationship exists between two variables 
+## it will appear as a pattern in the data. If you spot a pattern, ask yourself:
+
+## 1. Could this pattern be due to coincidence (i.e. random chance)?
+## 2. How can you describe the relationship implied by the pattern?
+## 3. How strong is the relationship implied by the pattern?
+## 4. What other variables might affect the relationship?
+## 5. Does the relationship change if you look at individual subgroups of the data?
+
+ggplot(data = faithful) + 
+  geom_point(mapping = aes(x = eruptions, y = waiting))
+## Patterns provide one of the most useful tools for data scientists because they reveal covariation. 
+## If you think of variation as a phenomenon that creates uncertainty, covariation is a phenomenon that reduces it. 
+## If two variables covary, you can use the values of one variable to make better predictions about the values of the second.
+## If the covariation is due to a causal relationship (a special case), 
+## then you can use the value of one variable to control the value of the second.
+
+ggplot(data = diamonds) +
+  geom_point(mapping = aes(x = carat, y = price))
+
+ggplot(data = diamonds) + 
+  geom_boxplot(mapping = aes(x = cut, y = price)) + 
+  facet_grid(. ~ carat)
+
+## The following code fits a model that predicts price from carat and then computes 
+## the residuals (the difference between the predicted value and the actual value). 
+## The residuals give us a view of the price of the diamond, once the effect of 
+## carat has been removed.
+library(modelr)
+
+mod <- lm(log(price) ~ log(carat), data = diamonds)
+
+diamonds2 <- diamonds %>% 
+  add_residuals(mod) %>% 
+  mutate(resid = exp(resid))
+
+ggplot(data = diamonds2) + 
+  geom_point(mapping = aes(x = carat, y = resid))
+
+## Once you’ve removed the strong relationship between carat and price, 
+## you can see what you expect in the relationship between cut and price: 
+## relative to their size, better quality diamonds are more expensive.
+ggplot(data = diamonds2) + 
+  geom_boxplot(mapping = aes(x = cut, y = resid))
+
+# 7.7 ggplot2 
+
+ggplot(data = faithful, mapping = aes(x = eruptions)) + 
+  geom_freqpoly(binwidth = .25)
+
+# saves typing, and, by reducing the amount of boilerplate, makes it easier to see 
+# what’s different between plots.
+
+# Rewriting the previous plot more concisely yields:
+ggplot(faithful, aes(eruptions)) + 
+  geom_freqpoly(binwidth = .25)
+
+# Sometimes we’ll turn the end of a pipeline of data transformation into a plot. Watch for the transition from %>% to +. 
+diamonds %>% 
+  count(cut, clarity) %>% 
+  ggplot(aes(clarity, cut, fill = n)) + 
+    geom_tile()
+
+# 7.8 Learning more
+#  https://amzn.com/331924275X
+# https://amzn.com/1449316956
+# http://www.cookbook-r.com/Graphs/
+# https://amzn.com/1498715230
 
 # ---------------------------------------------------------------------------------
-# Workflow: projects
+# 8 Workflow: projects
+# To handle these real life situations, you need to make two decisions:
+## What about your analysis is “real”, i.e. what will you save as your lasting record of what happened?
+## Where does your analysis “live”?
+
+# 8.1 What is real?
+
+
+
+
+
+
+
 
