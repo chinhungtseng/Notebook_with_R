94f8705fd5ad8f1dda8bcda88ccd2fd4e3904a34
chinhungtseng <bluesshown@gmail.com>
2019-03-17 23:43:18 +0800

    update

--- R_for_Data_Science/2_Wrangle.R
+++ R_for_Data_Science/2_Wrangle.R
@@ -3136,6 +3136,140 @@ stri_rand_strings(1000, 5, pattern = "[0-9]")
 stri_sort(c("hladny", "chladny"), locale="pl_PL")
 
 # --------------------------------------------------------------------------------------------------
+# 15 Factors
+
+# 15.1 Introduction
+# In R, factors are used to work with categorical variabels, variables that have a fixed and known set of possible values.
+# They are also useful when you want to display character vectors in a non-alphabetical order.
+
+# Historically, factors wewe much easier to work with than characters.
+# As a result, many of the functions in base R automatically convert characters to factors.
+# This means that factors often crop up in places where they're not actually helpful.
+# Fortunately, you don't need to worry about that in the tidyverse, and can focus on sutuations where factors are genuinely useful.
+
+# 15.1.1 Prerequisites
+
+# To work with factors, we'll use the forcats package, which provides tools for dealing with categorical variables (and it's an anagram of factors!).
+# It provides a wide range of helpers for working with factors.forcats is not part of the core tidyverse, so we need to load it explicitly.
+library(tidyverse)
+library(forcats)
+
+# 15.1.2 Learning more
+
+# If you want to learn more about factors, I recommend reading Amelia McNamara and Nicholas Horton's paper, 
+# Wrangling categorical data in R.(https://peerj.com/preprints/3163/)
+# This paper lays out some of the history discussed in stringAsFactors: An unauthorized biography (http://simplystatistics.org/2015/07/24/stringsasfactors-an-unauthorized-biography/)
+# and stringAsFactors = <sigh> (http://notstatschat.tumblr.com/post/124987394001/stringsasfactors-sigh),
+# and compares the tidy approaches to categorical data outlined in this book with base R methods. 
+# A early version of the paper help motivate and scope the forcats package; thanks Amelia & Nick!
+
+# 15.2 Creatin factors
+
+# Imagine that you have a variable that records month:
+x1 <- c("Dec", "Apr", "Jan", "Mar")
+
+# Using a sting to record this variable has two problems:
+# 1. There are only twelve possible months, and there's nothing saving you from typos:
+x2 <- c("Dec", "Apr", "Jam", "Mar")
+# 2. It doesn't sort in a useful way:
+sort(x1)
+
+# You can fix both of these problems with a factor. To create a factor you must start by creating a list of the valid levels:
+month_levels <- c(
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+)
+
+# Now you can create a factor:
+y1 <- factor(x1, levels = month_levels)
+y1
+sort(y1)
+
+# And any values not in the set will be silently converted to NA:
+y2 <- factor(x2, levels = month_levels)
+y2
+
+# If you want a warning, you can use readr::parse_factor():
+y2 <- parse_factor(x2, levels = month_levels)
+
+# If you omit the levels, they'll be taken from the data in alphabetical order:
+factor(x1)
+
+# Sometimes you'd prefer that the order of the levels match the order of the first appearance in the data.
+# You can do taht when creating the factor by setting levels to unique(x), or after the fact, with fct_inorder():
+f1 <- factor(x1, levels = unique(x1))
+f1
+
+f2 <- x1 %>% factor() %>% fct_inorder()
+f2
+
+# If you ever need to access the set of valid levels directly, you can do so with levels():
+levels(f2)
+
+# 15.3 General Social Survey
+
+# For the rest of this chapter, we're going to focus on forcats::gss_cat.
+# It's a sampe of data from the General Social Survey(http://gss.norc.org/), 
+# which is a long-running US survey conducted by the independent research
+# organzation NORC at the University of Chicago. The survey has thousands of questions, 
+# so in gss_cat I've selected a handful that
+# illustrate some common challenges you'll encounter when working with factors.
+gss_cat
+
+# (Rememeber, since this dataset is provided by a package, you can get more information about the variables with ?gss_cat .)
+# When factors are stored in a tibble, you can't see their levels so easily. One way to see them is with count():
+gss_cat %>% 
+  count(race)
+
+# Or with a bar chart:
+ggplot(gss_cat, aes(race)) + 
+  geom_bar()
+
+# By default, ggplot2 will drop levels that don't have any values. You can force them to display with:
+ggplot(gss_cat, aes(race)) + 
+  geom_bar() + 
+  scale_x_discrete(drop = FALSE)
+
+# These levels reapresent valid values that simply did not occur in this dataset.
+# Unfortunately, dplyr doesn't yet have a frop option, but it will in the future.
+
+# When working with factors, the two most common operations are changing the order of the levels, and changing the 
+# values of the levels. Those operations are described in the sections below.
+
+# 15.3.1 Exercise
+# 1. Explore the distribution of rincome (reported income). What makes the default bar chart hard to understand?
+#    How could you improve the plot?
+ggplot(gss_cat, aes(rincome)) + 
+  geom_bar() + 
+  theme(axis.text.x = element_text(angle = 90))
+
+ggplot(gss_cat, aes(rincome)) + 
+  geom_bar() + 
+  coord_flip()
+
+# 2. What is the most common relig in this survey? What's the most common partyid?
+gss_cat %>% 
+  count(relig, sort = TRUE)
+
+gss_cat %>% 
+  count(partyid, sort = TRUE)
+
+# 3. Which relig does denom (denomination) apply to ? How can you find out with a table?
+#    How can you find out with a visualisation?
+table(gss_cat$relig, gss_cat$denom) 
+
+gss_cat %>% 
+  ggplot(aes(relig, fill = denom)) + 
+  geom_bar()
+
+gss_cat %>% 
+  group_by(relig, denom) %>% 
+  count() %>% 
+  ggplot(aes(relig, denom, size = n)) + 
+  geom_point()
+
+# 15.4 Modifying factor order
+
 
 
 
