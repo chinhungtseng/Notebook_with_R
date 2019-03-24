30d06c647861644475675dd5f38a78be11b908fa
chinhungtseng <bluesshown@gmail.com>
2019-03-25 02:23:44 +0800

    update

--- R_for_Data_Science/x3_Program.R
+++ R_for_Data_Science/x3_Program.R
@@ -1182,11 +1182,199 @@ rm(`+`)
 # (http://adv-r.had.co.nz/)
 
 # ----------------------------------------------------------------------------------------------------------------------------
+# 20 Vectors
+
+# 20.1 Introduction
+
+# So far this book has focussed on tibbles and packages that work with them. But as you start to write your own functions, and dig deeper into R,
+# you need to learn about vectors, the objects that underline tibbles. If you've learned R in a more traditional way, you're probably already familiar with vectors,
+# as most R resources start with vectors and work their way up to tibbles. I think it's better to start with tibbles because they're immediately userful, 
+# and then work your way down to the underlying components.
+
+# Vectors are particularly important as most of the functions you will write will work with vectors.
+# It is possible to write functions that work with tibbles (like ggplot2, dplyr, and tidyr), but the tools you need to write such functions are currently
+# idiosyncratic and immature. I am working on a better approach, https://github.com/hadley/lazyeval, but it will not be ready in time for the publication of the book.
+# Even when complete, you'll still need to understand vectors, it'll just make it easier to write a user-friendly layer on top.
+
+# 20.2 Prerequisites
+
+# The focus of this chapter is on base R data structures, so isn't essential to load any packages.
+# We'll however, use a handful of functions from the purrr package to avoid some inconsistensies in base R.
+library(tidyverse)
+
+# 20.2 Vector basics
+
+# There are two types of vectors:
+# 1. Atomic vectors, of which there are six types: logical, integer, double, charactor, complex, and raw.
+#    Integer and double vectors are collectively know as nuberic vectors.
+# 2. Lists, which are sometimes called recursive recursive vectors because lists can contain other lists.
+
+# The chief difference between atomic vectors and lists is that atomic vectors are homegeneous, while lists can be heterogeneous.
+# There's one other related object: NULL. NULL is often used to represent the absence of a vector
+# (as opposed to NA wh ich is used to represent the absence of a value in a vector).
+# NULL typically behaves like a vector of length 0. Figure 20.1 summarises the interrelationships.
+
+# Vectors
+# ----------------------------------
+# |                                |   NULL
+# |  Atomic vectors                |
+# |  --------------------          |
+# |  |   logical        |          | 
+# |  |                  |          |
+# |  |    Numeric       |          |
+# |  |   -------------  |          |
+# |  |   |  Integer  |  |    List  |
+# |  |   |           |  |          |
+# |  |   |  Double   |  |          |
+# |  |   -------------  |          |
+# |  |                  |          |
+# |  |   Character      |          |
+# |  |                  |          |
+# |  --------------------          |
+# |                                |
+# ----------------------------------
+# Figure 20.1: The hierarchy of R's vector types
+
+# Every vector has two key properties:
+# 1. Its type, which you can determine with typeof().
+typeof(letters)
+typeof(1:10)
+
+# 2. Its length, which you can determine with length().
+x <- list("a", "b", 1:10)
+length(x)
+
+# Vectors can also contain arbitrary additional metadata in the form of attributes.
+# These attributes are used to create augmented vectors which build on additional behaviour.
+# There are three important types of argmented vector:
+# 1. Factors are built on top of integer vectors.
+# 2. Dates and date-times are built on top of numeric vectors.
+# 3. Data frames and tibbles are built on top of lists.
+
+# This chapter will introduce you to these important vectors from simplest to most complicated.
+# You'll start with atomic vectors, then build up to lists, and finish off with augmented vectors.
+
+# 20.3 Inportant types of atomic vecot 
+# The four most important typees of atomic vector are logical, integer, double, and character.
+# Raw and complex are rarely used during a data analysis, so I won't discuss them here.
+
+# Logical vectors are the simplest type of atomic vector because they can take only three possible values:
+# FALSE, TRUE, and NA. Logical vectors are usually constructed with comparison operators, as described in comparisons.
+# You can also create them by hand with c():
+1:10 %% 3 == 0
+c(TRUE, TRUE, FALSE, FALSE)
+
+# 20.3.2 Numeric 
+
+# Integer and double vectors are known collectively as numeric vectors.
+# In R, numbers are doubles by default. To make an integer, place an L after the number:
+typeof(1)
+typeof(1L)
+
+1.5L
+
+# The distinction between integers and doubles is not usually important, but there are two important differences that you should be aware of:
+# 1. Doubles are approximations. Doubles represent floating point numbers that can not always be preciesely represented with a fixed amount of memory.
+#    This means that you should consider all soubles to be approximations. 
+#    For example, what is square of the square root of two?
+x <- sqrt(2) ^ 2
+x
+
+x - 2
+
+#    This behaviour is common when working with floating point numbers: most calculations include some approximation error.
+#    Instead of comparing floating point numbers using ==, you should use dplyr::near() which allows for some numerical tolerance.
+
+# 2. Integers have one special value: NA, while doubles have four: NA, NaN, Inf and -Inf.
+#    All three special values NaN, Inf and -Inf can arise during division:
+c(-1, 0, 1) / 0
+
+# Avoid using == to check for these other special values.
+# Instead use the helper functions is.finite(), is.infinite(), and is.nan():
+# -------------------------------------------
+#                  0    Inf    NA    NaN
+# -------------------------------------------
+# is.finite()      x
+# -------------------------------------------
+# is.infinite()          x
+# -------------------------------------------
+# is.na()                       x      x
+# -------------------------------------------
+# is.nan()                             x
+# -------------------------------------------
+
+# 20.3.3 Character
+
+# Character vectors are the most complex type of atomic vector, because each element of a character vector is a sting, 
+# and a string can contain an arbitrary amount of data.
+
+# You 've already learned a lot about working with strings in strings. Here I wanted to mention one important feature of the underlying string implimentation:
+# R uses a global string pool. This means that each unique string is only stored in memory once, and every use of the string points to that representattion.
+# This reduces th amount of memory needed by duplicated strings. You can see this befauiour in practice with pryr::object_size():
+x <- "This is a reasonably long string."
+pryr::object_size(x)
+
+y <- rep(x, 1000)
+pryr::object_size(y)
+
+# y doesn't take up 1,000x as much memory as x, because each element of y is just a pointer to that same string. 
+# A pointer is 8 bytes, so 1000 pointers to a 136B sting is 8 * 1000 + 136 = 8.13 KB.
+
+# 20.3.4 Missing values
+
+# Note that each type of atomic vector has its own missing values:
+NA             # logical
+NA_integer_    # integer
+NA_real_       # double
+NA_character_  # character
+
+# Normally you don't need to know about these different types because you cna always use NA and it will be converted to the 
+# correct type using the implicit coercion rules described next.
+# However, there are some functions that are strict about their inputs, so it's useful to have this knowledgesitting in your back
+# pocket so you can be specific when needed.
+
+# 20.3.4 Exercises
+
+# 1. Describe th difference between is.finite(x) and !is.infinite(x).
+?is.infinite()
+## is.finite and is.infinite return a vector of the same length as x, indicating which elements are finite
+## (not infinite and not missing) or infinite.
+is.finite(c(0, NA, NaN, Inf, -Inf))
+is.infinite(c(0, NA, NaN, Inf, -Inf))
+
+# 2. Read the source code for dplyr::near() (Hint: to see the source code, drop the () ).
+#    How does it work?
+dplyr::near
+# function (x, y, tol = .Machine$double.eps^0.5) 
+# {
+#   abs(x - y) < tol
+# }
+# <bytecode: 0x108494778>
+# <environment: namespace:dplyr>
+
+## Instead of checking for exact equality, it checks that two numbers are within a certain tolerance, tol. 
+## By default the tolerance is set to the square root of .Machine$double.eps, 
+## which is the smallest floating point number that the computer can represent.
+
+# 3 . A logical vector can take 3 possible values. How many possible values can an integer vector take?
+#     How many possible values can a double take? Use google to do some research.
+.Machine$integer.max
 
 
 
 
 
+
+
+
+
+
+# 4. Brainstorm at least four functions that allow you to convert a double to an integer.
+#    How do they differ? Be precise.
+
+
+
+# 5. What functions from the readr package allow you to turn a string into logical, integer, and double vector?
 
 
 
